c91b55b8e13bc8a73ee6c19906808042dee36139
Highland Park 2823 <teamautomatons2823@gmail.com>
2015-01-20 16:52:14 -0600

    Revert "added code to set level of elevator to number of button (Y=1, B=2, A=3, X=4)"
    This reverts commit c703369c20607e979368f26c0d33b32a0be63ff6.

--- src/org/usfirst/frc/team2823/robot/Robot.java
+++ src/org/usfirst/frc/team2823/robot/Robot.java
@@ -21,17 +21,11 @@ public class Robot extends IterativeRobot {
 	//RobotDrive myRobot;
 	Joystick stick;
 	int autoLoopCounter;
-	Talon talon1;
-	Talon talon2;
-	Talon talon3;
-	Talon talon4;
-	Talon talon5;
+	Talon myTalon;
 	Encoder encoder;
 	DigitalInput di;
 	Gyro myGyro;
-	int newlevel;
-	int oldlevel;
-	final int R = 2;
+	
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
@@ -39,15 +33,10 @@ public class Robot extends IterativeRobot {
     public void robotInit() {
     	//myRobot = new RobotDrive(0,1);
     	stick = new Joystick(0);
-    	talon1 = new Talon(0);
-    	talon2 = new Talon(1);
-    	talon3 = new Talon(2);
-    	talon4 = new Talon(3);
-    	talon5 = new Talon(4);
+    	myTalon  = new Talon(0);
     	encoder = new Encoder(0, 1, true, EncodingType.k4X);
     	di = new DigitalInput(2);
     	myGyro = new Gyro(0);
-    	
     }
     
     /**
@@ -76,42 +65,16 @@ public class Robot extends IterativeRobot {
     public void teleopInit(){
     	encoder.reset();
     	myGyro.reset();
-    
     }
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-    	double axis1 = stick.getRawAxis(1);
-    	double axis3 = stick.getRawAxis(3);
-    	driveRobot(axis1, axis3);
-    	if (stick.getRawButton(6) == true)
-    	{	
-    		setElevatorLevel(newlevel);
-    	}
-    	if(stick.getRawButton(4) == true)
-    	{
-    		newlevel=1;
-    		
-    	}
-    	else if(stick.getRawButton(2) == true)
-    	{
-    		newlevel=2;
-    	}
-    	else if(stick.getRawButton(1) == true)
-    	{
-    		newlevel=3;
-    	}
-    	else if(stick.getRawButton(3) == true)
-    	{
-    		newlevel=4;
-    	}
-    	
-    	//DEBUG
-    	//System.out.printf("%f \t %f \n", axis1, axis3);
+    	double myAxis = stick.getRawAxis(0);
+    	myTalon.set(myAxis);
     	//System.out.printf("%b \n", di.get());
-    	//System.out.println("Gyro: " + myGyro.getAngle() + " Encoder: " + encoder.get());
+    	System.out.println("Gyro: " + myGyro.getAngle() + " Encoder: " + encoder.get());
         //myRobot.arcadeDrive(stick);
     }
     
@@ -122,29 +85,4 @@ public class Robot extends IterativeRobot {
     	LiveWindow.run();
     }
     
-    public void driveRobot(double a, double b)
-    {
-    	talon1.set(a);
-    	talon2.set(a);
-    	// Values are multiplied by -1 to ensure that the motors on the right spin opposite the motors on the left.
-    	talon3.set(-1*b);
-    	talon4.set(-1*b);
-    }
-    
-    public void setElevatorLevel(int l)
-    {
-    	try
-    	{
-    		int difference = newlevel - oldlevel;
-    		while(Math.abs(encoder.get()) < Math.abs((difference*14336)/(Math.PI*R)))
-    			{
-    				talon5.set(0.05*(Math.abs(difference)/difference));
-    			}
-    		oldlevel = newlevel;
-    	}
-    	catch(Exception e)
-    	{
-    		System.out.println("Nice try. You're already on the selected level.");
-    	}
-    }
 }
