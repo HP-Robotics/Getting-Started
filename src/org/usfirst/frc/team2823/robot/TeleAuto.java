0b0c5c6585a02f1e6724b9bb1841adb2e874606b
Highland Park 2823 <teamautomatons2823@gmail.com>
2015-04-01 21:23:28 -0500

    TeleAuto fixed
    Tele Auto is NOT an autonomous. The idea was to drive the robot in
    tele-op without humans. PID controllers are more precise anyways, so I
    thought getting rid of the fleshy meatbags would be fun.

--- src/org/usfirst/frc/team2823/robot/TeleAuto.java
+++ src/org/usfirst/frc/team2823/robot/TeleAuto.java
@@ -1,23 +1,12 @@
 package org.usfirst.frc.team2823.robot;
 
-public class TeleAuto implements AutoMode {
+public class TeleAuto{
 
 	Robot myBot;
 	
 	public TeleAuto (Robot myBot) {
 		this.myBot = myBot;
 	}
-	
-	@Override
-	public void autoInit() {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void autoPeriodic() {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
